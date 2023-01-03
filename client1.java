23124d379d196b406b6e65e673d23683e1926f85
niraj <121162012+niraj-achra@users.noreply.github.com>
2023-01-03 16:01:30 +0530

    final commit

--- client1.java
+++ client1.java
@@ -1,18 +1,34 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+
+import java.io.PrintStream;
 import java.net.*;
+import java.util.Scanner;
 
 public class client1{
     public static void main(String[] args) {
         try{
-            Socket s = new Socket("10.68.98.201",5000);
+           
+            Socket s = new Socket("localhost",8520);
             System.out.println("connected to server");
-            BufferedReader rd = new BufferedReader(new InputStreamReader(s.getInputStream()));
-            String st= rd.readLine();
-            System.out.println(st);
-            s.close();
-        }catch(Exception e){
+            while(true){
+           
+           
+           
+           
+            while(true){
+                Scanner sc1=new Scanner(System.in);
+                int num =sc1.nextInt();
+                PrintStream ps = new PrintStream(s.getOutputStream());
+                ps.println(num);
+                Scanner na= new Scanner(s.getInputStream());
+                String nb=na.nextLine();
+            System.out.println(nb);
+    } 
+}
+}
+     catch(Exception e){
            
         }
+    
+ 
     }
-}
+}  
--- server.java
+++ server.java
@@ -1,5 +1,5 @@
 import java.net.*;
-
+import java.util.Scanner;
 import java.io.*;
 
 public class server {
@@ -7,18 +7,34 @@ public class server {
     public static void main(String[] args) {
         
         try{
-        ServerSocket ss = new ServerSocket(5000);
+        ServerSocket ss = new ServerSocket(8520);
         System.out.println("server is running:::");
         while(true)
         {
             Socket s = ss.accept(); //to establish the connection
         System.out.println("connected");
-        PrintStream ps= new PrintStream(s.getOutputStream());
-        ps.print("hey avi how are you");
-        s.close();
-        ss.close();
-        } } catch (Exception e) {
-            System.out.println("connected");
+        
+        
+        
+        while(true){
+            Scanner sc1=new Scanner(s.getInputStream());
+            int num=sc1.nextInt();
+            String r;
+            if(num%2==0){
+              r="this no.is even no.";
+            }
+            else{
+               r="this no.is odd no.";
+            }
+            PrintStream ps=new PrintStream(s.getOutputStream());
+            ps.println(r);
+        
+        }
+    }
+       
+       
+         } catch (Exception e) {
+           
         }
     }
 }
